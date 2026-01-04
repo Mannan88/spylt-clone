58d8d4198718bfeb06efd60ae94a1ffe23134e0e
Mannan88 <kocharmanan88@gmail.com>
2026-01-04 21:27:27 +0530

    first commit, made hero, secondary and slider section

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import Navbar from "./components/Navbar"
+import Hero from "./sections/Hero"
+import gsap from "gsap"
+import { ScrollTrigger, ScrollSmoother } from "gsap/all"
+import MessageSection from "./sections/MessageSection"
+import FlavourSection from "./sections/FlavourSection"
+import { useGSAP } from "@gsap/react"
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+function App() {
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+    })
+  })
+
+  return (
+    <div>
+      <Navbar />
+
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <Hero />
+          <MessageSection />
+          <FlavourSection />
+           <div className="h-dvh border-2"></div>
+    </div>
+        </div>
+      </div>
+
+     
+  )
+}
+
+export default App
---
+++ src/components/FlavTitle.tsx
@@ -0,0 +1,73 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { SplitText } from "gsap/all"
+
+
+const FlavTitle = () => {
+
+  useGSAP(()=>{
+
+    const firstTxtSplit = SplitText.create(".first-text-split",{
+      type:"chars",
+    })
+    const secondTxtSplit = SplitText.create(".second-text-split",{
+      type:"chars",
+    })
+
+    gsap.from(firstTxtSplit.chars,{
+      duration:1,
+      yPercent:200,
+      stagger:0.02,
+      ease:"power1.inOut",
+      scrollTrigger:{
+        trigger:".flavor-section",
+        start:"top 30%",
+      }
+    });
+
+    gsap.to(".flavor-text-scroll",{
+      duration:1,
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        ease: "circ.out",
+        scrollTrigger:{
+          trigger:".flavor-section",
+          start:"top 10%",
+          markers:true,
+        }
+    })
+
+      gsap.from(secondTxtSplit.chars,{
+      duration:1,
+      yPercent:200,
+      stagger:0.02,
+      ease:"power1.inOut",
+      scrollTrigger:{
+        trigger:".flavor-section",
+        start:"top 1%",
+      }
+    });
+
+  })
+
+  return (
+    <div className="general-title col-center h-full 2xl:gap-32 xl:gap-24 gap-16" >
+        <div className="overflow-hidden 2xl:py-0 py-3 first-text-split">
+           <h1>We have 6</h1>
+      </div>
+      <div style={{
+      clipPath:"polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)"
+      }} className="flavor-text-scroll ">
+        <div className="bg-mid-brown px-3 pb-2">
+            <h2 className=" text-milk">
+                Freaking
+            </h2>
+        </div>
+      </div>
+      <div className="overflow-hidden 2xl:py-0 py-3 second-text-split">
+        delicious flavors
+      </div>
+  </div>
+  )
+}
+
+export default FlavTitle
